ab7e0273d540d38f9dffd293ebd3c8dddddc48a8
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-07-06 17:41:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
 
 def list_songs
   Song.all.sort { |a,b| a.name <=> b.name}.each_with_index(1) do |s, i|
-    puts "{i}."
+    puts "#{i}. "
 end
 
   def list_artists
